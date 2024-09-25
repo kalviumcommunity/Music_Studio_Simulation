f8246169f66b38716a69fd105f5e18a2f07820d6
Subham Mohanta <83577068+SubhamMohanta@users.noreply.github.com>
2024-09-25 09:17:48 +0530

    Update Equipment.java

--- src/Equipment.java
+++ src/Equipment.java
@@ -1,13 +1,1 @@
-public class Equipment {
-    private String type;
 
-    public Equipment(String type) {
-        // Correctly assign instance variables
-        this.type = type;
-    }
-
-    public void assignToSession() {
-        // Display equipment assignment
-        System.out.println(type + " is now assigned to a session.");
-    }
-}
