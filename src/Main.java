efe97edc80106448e24fc3b2abe957cfdbfb14a6
Subham Mohanta <83577068+SubhamMohanta@users.noreply.github.com>
2024-09-25 09:18:01 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,1 @@
-public class Main {
-    public static void main(String[] args) {
-        // Create objects for musicians, engineers, and equipment
-        Musician musician1 = new Musician("John", "Guitar");
-        Musician musician2 = new Musician("Emily", "Piano");
-        
-        Engineer soundEngineer = new Engineer("Lucas", "Sound Engineer");
-        Equipment mic = new Equipment("Microphone");
 
-        // Demonstrate basic actions by calling methods on the objects
-        musician1.record();
-        musician2.record();
-        
-        soundEngineer.mixTrack();
-        
-        mic.assignToSession();
-    }
-}
