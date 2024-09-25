6dbdbe65eebd445c938e887ea98655688e0d80f0
Subham Mohanta <83577068+SubhamMohanta@users.noreply.github.com>
2024-09-25 09:17:30 +0530

    Update Engineer.java

--- src/Engineer.java
+++ src/Engineer.java
@@ -1,15 +1,1 @@
-public class Engineer {
-    private String name;
-    private String role;
 
-    public Engineer(String name, String role) {
-        // Correctly assign instance variables
-        this.name = name;
-        this.role = role;
-    }
-
-    public void mixTrack() {
-        // Display mixing message
-        System.out.println(name + " is mixing the track.");
-    }
-}
